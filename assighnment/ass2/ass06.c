3da2b72c01255de48f137dea5f3faf5251587036
Ratnesh Tiwari <ratneshvandanatiwari21@gmail.com>
2026-09-17 20:43:56 +0530

    17/09

--- assighnment/ass2/ass06.c
+++ assighnment/ass2/ass06.c
@@ -13,9 +13,12 @@ int main(){
     scanf("%d",&year);
 
     if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-        printf("%d is a Leap Year ",year);
+        printf("%d is a Leap Year \n",year);
     else
-        printf("%d is not a Leap Year ",year);
+        printf("%d is not a Leap Year \n",year);
+    
+    ((year % 400 == 0) ||(year % 4 == 0 && year % 100 != 0))? printf("Leap Year") : printf("Not a Leap Year");
+
 
     return 0;
 }
